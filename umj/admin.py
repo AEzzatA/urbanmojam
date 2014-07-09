cb48a1f2cb044c2a87be7e57deffc3ef37269ad1
Ahmed Ezzat <AEzzatA@gmail.com>
2014-07-10 01:50:22 +0200

    Enhanced Admin page Added pytz to requirements

--- umj/admin.py
+++ umj/admin.py
@@ -13,6 +13,10 @@ class TermAdmin(admin.ModelAdmin):
 	]
 	
 	inlines = [DefInline]
+	list_display = ('phrase', 'pub_date', 'was_published_recently')
+	list_filter = ('pub_date',)
+	search_field = 'phrase'
+	date_hierarchy = 'pub_date'
 
 
 admin.site.register(Term, TermAdmin)
