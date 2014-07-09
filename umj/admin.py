145a4a7ec072be96412e702125be33cc64bec5d8
Ahmed Ezzat <AEzzatA@gmail.com>
2014-07-09 18:43:39 +0200

    Added model and basic admin page

--- umj/admin.py
+++ umj/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Term, Definition
 
-# Register your models here.
+class DefInline(admin.TabularInline):
+	model = Definition
+	extra = 3
+
+
+class TermAdmin(admin.ModelAdmin):
+	fieldsets = [
+	(None,               {'fields': ['phrase']}),
+	('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+	]
+	
+	inlines = [DefInline]
+
+
+admin.site.register(Term, TermAdmin)
--- umj/models.py
+++ umj/models.py
@@ -1,3 +1,77 @@
+import datetime
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+#TODO:
+#1 Change Definition class name it doesn't look good
+#2 Change meaning name to meaning and Field to TextField() not char field
+#3
+
+class Term(models.Model):
+	'''
+	Term/Phrase model. that needs to be defined
+	phrase -> CharField
+	pub_date -> DateTimeField
+	'''
+	phrase =  models.CharField(max_length = 140)
+	pub_date = models.DateTimeField('date added')
+
+	def __unicode__(self):
+		return self.phrase
+	def was_published_recently(self):
+		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Definition(models.Model):
+	''' Term definition
+	term -> CharField
+	country -> CharField | country code
+	'''
+	term = models.ForeignKey(Term)
+	meaning = models.TextField()
+	votes = models.IntegerField(default=0)
+	counrty_list = (
+		('DZ', 'Algeria'),
+		('BH', 'Bahrain'),
+		('TD', 'Chad'),
+		('KM', 'Comoros'),
+		('DJ', 'Djibouti'),
+		('EG', 'Egypt'),
+		('ER', 'Eritrea'),
+		('IQ', 'Iraq'),
+		('JO', 'Jordan'),
+		('KW', 'Kuwait'),
+		('LB', 'Lebanon'),
+		('LY', 'Libya'),
+		('MR', 'Mauritania'),
+		('MA', 'Morocco'),
+		('OM', 'Oman'),
+		('PS', 'Palestine'),
+		('QA', 'Qatar'),
+		('EH', 'SADR'),
+		('SA', 'Saudi Arabia'),
+		('SO', 'Somalia'),
+		('SD', 'Sudan'),
+		('SY', 'Syria'),
+		('TN', 'Tunisia'),
+		('AE', 'United Arab Emirates'),
+		('YE', 'Yemen')
+		)
+	country = models.CharField(max_length=2,
+		choices=counrty_list,
+		default='EG')
+
+	pub_date = models.DateTimeField('date added definition')
+
+	def __unicode__(self):
+		return "{0} {1}".format(self.meaning, self.country)
+
+	def was_published_recently(self):
+		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Tag(models.Model):
+	''' Tags that realates to term definition'''
+	terms = models.ManyToManyField(Term)
+	tag_name = models.CharField(max_length=15)
